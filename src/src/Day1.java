68a3f521d73ae2b6c9a8d196c7d56552c90094f7
Michael Piechota <mepiechota@gmail.com>
2017-07-05 17:59:43 -0700

    Completed Day 1 Challenge

---
+++ src/src/Day1.java
@@ -0,0 +1,25 @@
+/*
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World.
+on a single line, and finally print the value of your variable on a second line.
+*/
+
+import java.util.*;
+
+public class Day1 {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+        Scanner scan = new Scanner(System.in);
+
+        // Read a full line of input from stdin and save it to our variable, inputString.
+        String inputString = scan.nextLine();
+
+        // Close the scanner object, because we've finished reading
+        // all of the input from stdin needed for this challenge.
+        scan.close();
+
+        // Print a string literal saying "Hello, World." to stdout.
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
+
