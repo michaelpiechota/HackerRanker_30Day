7a904e917568a3eff083325d3381555528bc8d25
Michael Piechota <mepiechota@gmail.com>
2017-07-06 09:57:49 -0700

    Completed Day 3 challenge.

--- Challenges/src/Day3.java
+++ Challenges/src/Day3.java
@@ -1,7 +1,27 @@
 /*
-Operators
-Challenge unlocks on 07/05/2017
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip),
+and tax percent (the percentage of the meal price being added as tax) for a meal,
+find and print the meal's total cost.
 */
 
+import java.util.*;
+
 public class Day3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+
+        // Write your calculation code here.
+        double tipAdded = (mealCost + (mealCost * (tipPercent*0.01)));
+        double costAfterTax = (tipAdded + (mealCost * (taxPercent*0.01)));
+
+        // cast the result of the rounding operation to an int and save it as totalCo10st
+        int totalCost = (int) Math.round(costAfterTax);
+
+        // Print your result
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
 }
